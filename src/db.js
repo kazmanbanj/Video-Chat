399579a37e8dd9eb192b365e003b48f4ae259361
Banjoko Kazeem <banjoko10@gmail.com>
2021-08-06 00:09:04 +0100

    signing up on fb

---
+++ src/db.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+let firebaseConfig = {
+    apiKey: process.env.VUE_APP_apiKey,
+    authDomain: process.env.VUE_APP_authDomain,
+    projectId: process.env.VUE_APP_projectId,
+    storageBucket: process.env.VUE_APP_storageBucket,
+    messagingSenderId: process.env.VUE_APP_messagingSenderId,
+    appId: process.env.VUE_APP_appId,
+    measurementId: process.env.VUE_APP_measurementId
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAnalytics = firebase.analytics();
+
+export default firebaseApp.firestore();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Home page</h1>
+    <p>Welcome {{ user }}</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import db from '../db.js'
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  name: 'Home',
+  data() {
+    return {
+      user: null
+    }
   },
-};
+  mounted() {
+    db.collection('users')
+    .doc('nm6oG41I9ndSpxCWYTlu')
+    .get()
+    .then(
+      snapshot => {
+        this.user = snapshot.data().name
+      }
+    )
+  }
+}
 </script>
